cupcake99/PushyPushPush

commit cd07f74c0aee04ede88c66c979b471e37868439a

Initial attempt to make modes more 'modular'
- Add new methods to register a 'mode spec' into the mode object
  and to return the mode by attaching it to a CC number (for a
  button on the Push)
- Make new method to change mode and allow paging on modes
- Remove horrid object parent indirection reference access nonsense
- Start to clean up syntax for style (lua style of no parentheses
  where possible, make sure to only use colon when needing self etc)
- Other stuff?

state.lua
@@ -1,9 +1,28 @@
+local note_table = {
+    ["C"] = 0,
+    ["D"] = 1,
+    ["E"] = 2,
+    ["F"] = 3,
+    ["G"] = 4,
+    ["A"] = 5,
+    ["B"] = 6,
+    ["O"] = 7,
+    [0] = "C",
+    [1] = "D",
+    [2] = "E",
+    [3] = "F",
+    [4] = "G",
+    [5] = "A",
+    [6] = "B",
+    [7] = "OFF",
+}
+
 class "State"
 -- representation of the state of the Renoise song and methods to change parameters within it
 
-function State:__init (parent)
-    self.push = parent
+function State:__init ()
     self.activeMode = nil
+    self.activePage = 0
     self.activeSeqIndex = nil
     self.activePattern = nil
     self.activeTrack = nil
@@ -21,8 +40,8 @@ function State:__init (parent)
         }
     }
     self.displaySysexByLine = {}
-    self.current = table.rcopy(Push.control)
-    self.last = table.rcopy(self.current)
+    self.current = setmetatable({}, {__index = Push.control})
+    self.last = table.copy(self.current)
     self.shiftActive = false
     self.dirty = false
     self.noteOns = {}
@@ -59,15 +78,33 @@ function State:getInstrumentCount ()
     if self.instrumentCount ~= instrumentCt then self.instrumentCount = instrumentCt end
 end
 
+function State:loadMode (cc)
+    local page, mode = 1, modes:select(cc)
+    if mode then
+        if mode.name == self.activeMode.name then
+            if self.activePage == #mode.page then page = 1 else
+                page = self.activePage + 1 <= #mode.page and self.activePage + 1
+            end
+            if not page then return end --see if page can change, if not mode and page is already loaded so exit
+        end
+        for i = 1, 120 do
+            self.current[i] = nil
+            if mode.page[page][i] then
+                self.current[i] = mode.page[page][i]
+            end
+        end
+        mode.page[page].display()
+        self.activeMode = {name = mode.name, action = mode.page[page].action}
+        self.activePage = page
+        return true
+    end
+    return false
+end
+
 function State:changeMode (data)
-    if not self.activeMode then return false end
-    local mode = self.push.modes.select[data[2]]
-    if not mode then return false end
-    -- if self.push.modes.select[data[2]].cc == data[2] then return false end
-    if self.activeMode.name ~= mode.name and data[3] > 1 then
-        self.activeMode = mode
-        self.activeMode.lights(self.push.modes)
-        self.activeMode.display(self.push.modes)
+    if data[3] > 1 then
+        if not self:loadMode(data[2]) then return false end
+        self:setPatternDisplay {0,0,1}
         self.dirty = true
     end
     return true
@@ -122,7 +159,7 @@ function State:setEditPos (data)
     if pos == nil then return false end
     local shift_mult = (self.shiftActive and 10) or 1
     if data[2] == 15 then
-        local encoderVal = self.push.midi:encoderParse(data, 7)
+        local encoderVal = midi:encoderParse(data, 7)
         if encoderVal == 0 then return nil end
         pos.line = pos.line + encoderVal * shift_mult
     elseif (data[2] == 46 or data[2] == 47) and data[3] > 0 then
@@ -201,7 +238,7 @@ function State:setPatternDisplay (data)
         for i = 1, song.patterns[patt].number_of_lines do
             if song.patterns[patt].tracks[trk].lines[i]:note_column(1).note_string ~= "---" then
                 line = song.patterns[patt].tracks[trk].lines[i]:note_column(1)
-                note = self.note_table[string.sub(line.note_string, 1, 1)]
+                note = note_table[string.sub(line.note_string, 1, 1)]
                 self.current[(92 - ((i - 1) * 8)) + 128 + note].value = Push.light.pad.super_blue
             end
         end
@@ -232,7 +269,7 @@ function State:setPatternDisplay (data)
         for i = pos - 3, pos + 4 do
             if song.patterns[patt].tracks[trk].lines[i] then
                 line = song.patterns[patt].tracks[trk].lines[i]:note_column(1)
-                note = self.note_table[string.sub(line.note_string, 1, 1)]
+                note = note_table[string.sub(line.note_string, 1, 1)]
             end
             if note then self.current[(92 - (j * 8)) + 128 + note].value = Push.light.pad.super_blue end
             j = j + 1
@@ -240,25 +277,6 @@ function State:setPatternDisplay (data)
     end
 
 end
-
-State.note_table = {
-    ["C"] = 0,
-    ["D"] = 1,
-    ["E"] = 2,
-    ["F"] = 3,
-    ["G"] = 4,
-    ["A"] = 5,
-    ["B"] = 6,
-    ["O"] = 7,
-    [0] = "C",
-    [1] = "D",
-    [2] = "E",
-    [3] = "F",
-    [4] = "G",
-    [5] = "A",
-    [6] = "B",
-    [7] = "OFF",
-}
 
 -- this is just for the nice touchy lights. hopefully one day it will also help playing notes into renoise
 function State:receiveNote (data)
@@ -275,7 +293,7 @@ end
 function State:setMasterVolume (data)
     local shift_mult = (self.shiftActive and 0.1) or 1
     local masterTrack = song.tracks[song.sequencer_track_count + 1]
-    local val = masterTrack.postfx_volume.value + ((self.push.midi:encoderParse(data, 5) * 0.2) * shift_mult)
+    local val = masterTrack.postfx_volume.value + ((midi:encoderParse(data, 5) * 0.2) * shift_mult)
     if val < masterTrack.postfx_volume.value_min then val = masterTrack.postfx_volume.value_min
     elseif val > masterTrack.postfx_volume.value_max then val = masterTrack.postfx_volume.value_max end
     masterTrack.postfx_volume.value = val
@@ -333,7 +351,7 @@ function State:setActiveTrack ()
             end
             z = z + 1
         end
-        self:setPatternDisplay({0, 0, 1})
+        self:setPatternDisplay {0, 0, 1}
         self.dirty = true
     end
 end
@@ -357,7 +375,7 @@ function State:changeSequence (data)
     -- if song.transport.follow_player then
     --     pos = song.transport.playback_pos
     -- end    local pos = (song.transport.follow_player and song.transport.playback_pos) or nil
-    local direction = self.push.midi:encoderParse(data, nil)
+    local direction = midi:encoderParse(data, nil)
     if direction == 0 then return end
     if direction >= 1 then
         if self.activeSeqIndex == song.transport.song_length.sequence then return end
@@ -388,7 +406,7 @@ end
 
 function State:changePattern (data)
     if data[3] == 0 then return end
-    local direction = self.push.midi:encoderParse(data, nil)
+    local direction = midi:encoderParse(data, nil)
     if direction == 0 then return end
     if direction >= 1 then
         if self.activePattern == 999 then return end
@@ -401,7 +419,7 @@ end
 
 function State:changePatternLength (data)
     if data[3] == 0 then return end
-    local direction = self.push.midi:encoderParse(data, 3)
+    local direction = midi:encoderParse(data, 3)
     if direction == 0 then return end
     local lines = song.patterns[self.activePattern].number_of_lines
     local shift_mult = (self.shiftActive and 4) or 1
@@ -429,7 +447,7 @@ function State:changeTrack (data)
     else
         local direction
         -- if data[2] == 71 then
-        --     direction = self.push.midi:encoderParse(data, 8)
+        --     direction = midi:encoderParse(data, 8)
         -- else
         direction = (data[2] == 45 and 1) or -1
         -- end
@@ -451,7 +469,7 @@ end
 
 function State:changeInstrument (data)
     if data[3] == 0 then return end
-    local direction = self.push.midi:encoderParse(data, 8)
+    local direction = midi:encoderParse(data, 8)
     if direction == 0 then return end
     if direction >= 1 then
         if self.activeInstrument == renoise.Song.MAX_NUMBER_OF_INSTRUMENTS
@@ -538,7 +556,7 @@ function State:insertNote (data)
     local trk = self.activeTrack
     local delta = os.clock()
     if data[1] == 144 then
-        note = self.note_table[(data[2] - 36) % 8]
+        note = note_table[(data[2] - 36) % 8]
         line = math.floor((99 - data[2]) / 8)
         if self.noteDelta and (delta - self.noteDelta) < 0.4 then
             -- print(delta - self.noteDelta)

main.lua
@@ -3,27 +3,33 @@
 tool = renoise.tool()
 song = nil
 push = nil
+state = nil
+modes = nil
+midi = nil
 
-function letsGo ()
+local function letsGo ()
     if not song then song = renoise.song() end
     if not tool then tool = renoise.tool() end
-
     require "utils"
     require "push"
     require "state"
     require "modes"
     require "midi"
-
     if not push then push = Push() end
+    if not state then state = State() end
+    if not modes then modes = Modes() end
+    if not midi then midi = Midi() end
     push:start()
 end
 
-function goodnight ()
+local function goodnight ()
     push:stop()
-
     tool = nil
     song = nil
     push = nil
+    state = nil
+    modes = nil
+    midi = nil
 end
 
 tool.app_new_document_observable:add_notifier(letsGo)

midi.lua
@@ -1,8 +1,7 @@
 class "Midi"
 -- library of static functions and tables for MIDI communication and formatting
 
-function Midi:__init (parent)
-    self.push = parent
+function Midi:__init ()
     -- self.server = nil
     -- self.client = nil
     -- self.address = Midi.OSCAddress
@@ -58,7 +57,7 @@ Midi.sysex = {
     user_mode = { 240, 71, 127, 21, 98, 0, 1, 1, 247 }
 }
 
-function Midi:handleMidi (data)
+function Midi.handleMidi (data)
     assert(#data == 3)
     local control
     if data[1] == Midi.status.note_on then control = getControlFromType("note", data[2])
@@ -66,35 +65,35 @@ function Midi:handleMidi (data)
     -- if not control then return end
     -- rprint(control)
     if control and control.hasCC then
-        if control.name == "shift" then self.push.state:shift(data) return end
-        if control.name == "play" then self.push.state:play(data) return end
-        if control.name == "record" then self.push.state:edit(data) return end
-        if self.push.state:changeMode(data) then return end
+        if control.name == "shift" then state:shift(data) return end
+        if control.name == "play" then state:play(data) return end
+        if control.name == "record" then state:edit(data) return end
+        if state:changeMode(data) then return end
     end
-    self.push.state.activeMode.action(self.push.modes, data)
+    state.activeMode.action(data)
 end
 
-function Midi:sendMidi (data)
-    if self.push.output.is_open then
-        self.push.output:send(data)
+function Midi.sendMidi (data)
+    if push.output.is_open then
+        push.output:send(data)
     end
 end
 
-function Midi:encoderParse (data, thinningLevel)
+function Midi.encoderParse (data, thinningLevel)
     if data[3] == 0 then return 0 end
-    if #self.push.encoderStream ~= 0 and self.push.encoderStream[#self.push.encoderStream].cc ~= data[2] then
-        self.push.encoderStream = {}
+    if #push.encoderStream ~= 0 and push.encoderStream[#push.encoderStream].cc ~= data[2] then
+        push.encoderStream = {}
     end
     if thinningLevel then
-        table.insert(self.push.encoderStream, {cc = data[2], value = data[3]})
-        if #self.push.encoderStream == 1 then
-            if self.push.encoderStream[1].value < 64 then
-                return self.push.encoderStream[1].value
+        table.insert(push.encoderStream, {cc = data[2], value = data[3]})
+        if #push.encoderStream == 1 then
+            if push.encoderStream[1].value < 64 then
+                return push.encoderStream[1].value
             else
-                return -1 * (128 - self.push.encoderStream[1].value)
+                return -1 * (128 - push.encoderStream[1].value)
             end
-        elseif #self.push.encoderStream == thinningLevel then
-            self.push.encoderStream = {}
+        elseif #push.encoderStream == thinningLevel then
+            push.encoderStream = {}
         end
         return 0
     else
@@ -102,21 +101,9 @@ function Midi:encoderParse (data, thinningLevel)
     end
 end
 
-function Midi:writeText (data, ...)
-    local n_args = select('#', ...)
-    if data then
-        self:sendMidi(data)
-        if n_args > 0 then
-            local t = {...}
-            local line = t[1]
-            self.push.state.displaySysexByLine[line] = data
-        end
-    end
-end
-
 -- format sysex table for writing to display. Byte 5 is always line number. Can write up to 68 characters long, byte values 0-127.
 -- Function is variadic, (format, text, line, zone). First three are required, zone is optional.
-function Midi:formatLine (format, text, ...)
+function Midi.formatLine (format, text, ...)
     local s = table.copy(format)
     local length = string.len(text)
     local n_args = select('#', ...)
@@ -153,18 +140,30 @@ function Midi:formatLine (format, text, ...)
     end
 end
 
+function Midi:writeText (data, ...)
+    local n_args = select('#', ...)
+    if data then
+        self.sendMidi(data)
+        if n_args > 0 then
+            local t = {...}
+            local line = t[1]
+            state.displaySysexByLine[line] = data
+        end
+    end
+end
+
 -- clear the whole display. Takes an object as argument (the Push object), to access MIDI operations. Line number is optional.
 function Midi:clearDisplay (...)
     local m = {}
     if select('#', ...) == 1 then
         m = table.copy(Midi.sysex.clear_line)
         m[5] = Midi.sysex.line_number.clear[select(1, ...)]
-        self:sendMidi(m)
+        self.sendMidi(m)
     else
         for i = 1, 4 do
             m[i] = table.copy(Midi.sysex.clear_line)
             m[i][5] = Midi.sysex.line_number.clear[i]
-            self:sendMidi(m[i])
+            self.sendMidi(m[i])
         end
     end
 end

commit 7b6b136211a14c028d83e5d0141d7309ce68cdeb

Fix bug introduced in cd07f74 in MIDI parsing
- The changes to State:changeMode made in the commit
  meant that it was eating most MIDI messages, and not
  returning false when it should have been, causing
  controls to seem slow or not working at all.

manifest.xml
@@ -3,7 +3,7 @@
   <ApiVersion>6</ApiVersion>
   <AutoUpgraded>true</AutoUpgraded>
   <Id>lil.cupcake.PushyPushPush</Id>
-  <Version>0.009</Version>
+  <Version>0.010</Version>
   <Author>cupcake99</Author>
   <Name>PushyPushPush</Name>
   <Category>Utility</Category>

state.lua
@@ -109,12 +109,14 @@ function State:loadMode (cc)
 end
 
 function State:changeMode (data)
-    if data[3] > 1 then
+    if modes.modes[data[2]] and data[3] > 1 then
         if not self:loadMode(data[2]) then return false end
+        self:loadMode(data[2])
         self:setPatternDisplay {0,0,1}
         self.dirty = true
+        return true
     end
-    return true
+    return false
 end
 
 function State:shift (data)